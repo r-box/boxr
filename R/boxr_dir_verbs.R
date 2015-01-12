94c0213b01943e8adcce18152e34229354fef217
Brendan Rocks <rocks.brendan@gmail.com>
2015-01-11 17:39:00 -0800

    Added a basic s3 class, broke up the files

---
+++ R/boxr_dir_verbs.R
@@ -0,0 +1,131 @@
+#' Recursive, Directory-wide Operations to Synchronize Local and box.com
+#' directories
+#' 
+#' These functions take a path to a local directory, and a box.com folder id,
+#' and perform the update/sychronization operations.
+#' 
+#' \code{box_fetch} Will create local versions of files and directories which
+#' are present on box.com, but not locally. If \code{overwrite} is true, files
+#' which are present both locally and on box.com will be overwritten with the 
+#' box.com versions.
+#' 
+#' \code{box_push} Will create box.com versions of files and directories which
+#' are present locally, but not on box.com. Files which already appear to exist
+#' will be uploaded as new versions.
+#' 
+#' \code{box_fetch} A convenience function, effectively 
+#' \code{box_fetch();box_push()}
+#' 
+#' @aliases box_push box_fetch
+#' 
+#' @param recursive \code{logical}. Should the call include subdirectories and 
+#' thier contents?
+#' @param ignore_dots \code{logical}. Should local directories with filenames
+#' begining with dots be ignored? This is useful for 'invisible' folders such as
+#' \code{.git} and \code{.Rproj.user} where uploading them is likely to be
+#' unexpected.
+#' 
+#' @details The box.com API does not have direct support for downloading more 
+#' than one file. With \code{recursive} set to \code{false}, \code{box_pull} 
+#' will download the files, but not subdirectories of the folder specified by 
+#' \code{dir_id}. If \code{recursive == TRUE}, then it will download every file 
+#' and folder in the directory tree. Because R has to make recursive API calls 
+#' to explore the directory structure, and then iterate through each file it 
+#' finds, this option can be rather slow.
+#' 
+#' @inheritParams dirTreeRecursive 
+#' @inheritParams box_dl
+#' 
+#' @return Nothing. Used for its side-effects.
+box_fetch <- 
+  function(
+    dir_id, local_dir = getwd(),
+    recursive = TRUE,
+    overwrite = TRUE
+  ){
+    if(!recursive){
+      downloadDirFiles(dir_id, local_dir)
+      # You need to insert a message here
+      return(invisible(TRUE))
+    }
+    # 1. Recursively scan the box dir for folders
+    d <- dirTreeRecursive(dir_id)
+    
+    # 2. Update the files in the tld
+    downloadDirFiles(dir_id, local_dir, overwrite = overwrite)
+    
+    # Loop through the box dirs. If they don't exist, create them.
+    # Once they do, fill 'em up!
+    for(i in 1:nrow(d)){
+      dir.create(d$local_dir[i], showWarnings = FALSE)
+      downloadDirFiles(d$id[i], d$local_dir[i], overwrite = overwrite)
+    }
+  }
+
+
+#' @rdname box_fetch
+box_push <- 
+  function(dir_id, local_dir = getwd(), ignore_dots = TRUE){
+    
+    local_dirs <- list.dirs()[-1]
+    if(ignore_dots)
+      local_dirs <- local_dirs[!grepl("^\\.\\/\\.", local_dirs)]
+    
+    dir_depth <- 
+      unlist(
+        lapply(
+          gregexpr("\\/", local_dirs),
+          function(x) length(attr(x, "match.length"))
+        )
+      )
+    
+    # Order the dirs by depth
+    local_dirs <- local_dirs[order(dir_depth)]
+    
+    # A version where the dir names will be replaced with thier box.com ids
+    box_dirs <- gsub("^\\.\\/", paste0(dir_id, "/"), local_dirs)
+    
+    for(i in 1:length(box_dirs)){
+      new_dir <-
+        box_dir_create(
+          dir_name = basename(box_dirs[i]),
+          parent_dir_id = 
+            gsub(
+              ".*/", "", 
+              gsub(
+                paste0("/", basename(box_dirs)[i]), "", box_dirs[i]
+              )
+            )
+        )
+      
+      # If the folder is brand new, take it's id
+      if(new_dir$status == 201){
+        new_dir_id <- httr::content(new_dir)$id
+        message(
+          "Created box.com folder (id: ", new_dir_id, ") ", local_dirs[i]
+        )
+      }
+      # If the folder already exists, take it's id
+      if(new_dir$status == 409)
+        new_dir_id <- httr::content(new_dir)$context_info$conflicts[[1]]$id
+      
+      # Create a string, where the name of the local dir is replaced by it's
+      # box.com folder id
+      rep_str <- gsub(basename(box_dirs[i]), new_dir_id, box_dirs[i])
+      
+      # Where you see the old path, replace it
+      box_dirs <- gsub(box_dirs[i], rep_str, box_dirs)
+      
+      # Upload the files in the directory
+      uploadDirFiles(
+        new_dir_id, 
+        paste0(local_dir, gsub("^\\.", "", local_dirs[i]))
+      )
+    }
+  }
+
+#' @rdname box_fetch
+box_merge <- function(dir_id, local_dir = getwd(), ignore_dots = TRUE){
+  box_push(local_dir, dir_id, ignore_dots = ignore_dots)
+  box_fetch(local_dir, dir_id)
+}
---
+++ R/boxr_internals.R
@@ -0,0 +1,266 @@
+#' Upload or update a file
+#' 
+#' Internal functions used by \code{\link{box_ul}} (who's use is recommended as
+#' an alternative to these functions).
+#' 
+#' The box.com api requires different API calls to upload a new file, and to
+#' upload a new version of a file which already exists (incrementing the version
+#' number).
+#' 
+#' \code{box_upload_new} make the API call to upload a new file.
+#' \code{box_update_file} makes the API call to update an existing file.
+#' 
+#' @aliases box_update_file
+#' @param file A path to a file stored locally
+#' @param file_id the box.com id of the file you'd like to update
+#' @param dir_id The box.com id for the folder that you'd like to upload to
+#' @return The \code{\link{httr}} object returned by the api call
+#' @keywords internal
+box_upload_new <- function(file, dir_id){
+  
+  httr::POST(
+    "https://upload.box.com/api/2.0/files/content",
+    httr::config(token = getOption("boxr.token")),
+    encode = "multipart",
+    body = 
+      list(
+        attributes = 
+          paste0(
+            '{"name": "', basename(file), '", "parent": {"id":"', dir_id,'"}}'
+          ),
+        file       = httr::upload_file(file)
+      )
+  )
+}
+
+
+#' @rdname box_upload_new
+#' @keywords internal
+box_update_file <- function(file, file_id, dir_id){
+  httr::POST(
+    paste0("https://upload.box.com/api/2.0/files/", file_id, "/content"),
+    httr::config(token = getOption("boxr.token")),
+    encode = "multipart",
+    body = 
+      list(
+        attributes = 
+          paste0(
+            '{"name": "', basename(file), '", "parent": {"id":"', dir_id,'"}}'
+          ),
+        file = httr::upload_file(file)
+      )
+  )
+}
+
+
+
+
+
+#' Create a data.frame of metadata of the contents of a local directory
+#' 
+#' @inheritParams dirTreeRecursive
+#' @return A data.frame of metadata.
+#' @keywords internal
+create_loc_dir_df <- function(local_dir = getwd()){
+  fs <- list.files(local_dir, full.names = TRUE)
+  if(length(fs) < 1L)
+    return(NULL)
+  
+  # Create a data.frame of metadata on the contents
+  # of a local directory
+  # You need to make this fail gracefully if the home directory is
+  # empty (e.g.) return null
+  
+  fi <- file.info(fs)
+  
+  df <- 
+    data.frame(
+      name = fs,
+      fi,
+      row.names = NULL,
+      stringsAsFactors = FALSE
+    )
+  
+  df$type <- ifelse(df$isdir, "folder", "file")
+  df$sha1 <- NA
+  
+  if(sum(!df$isdir) > 0L){
+    sha1 <-
+      sapply(
+        df$name[!df$isdir], 
+        function(f)
+          digest::digest(f, "sha1", file = TRUE)
+      )
+    
+    df$sha1 <- sha1[df$name]
+  }
+  
+  return(df)
+}
+
+
+
+#' Single Directory Operations for Downloading and Uploading mutiple Files
+#' 
+#' Download or upload the contents of a box.com directory, not including 
+#' subdirectories
+#' 
+#' @aliases uploadDirFiles
+#' 
+#' @inheritParams dirTreeRecursive
+#' 
+#' @return \code{TRUE} for a successful sync, \code{NULL} if the box.com folder 
+#' is empty.
+#' @keywords internal
+downloadDirFiles <- function(dir_id, local_dir = getwd(), overwrite = TRUE){
+  
+  loc_dir_df <- create_loc_dir_df(local_dir)
+  box_dir_df <- box_ls(dir_id)
+  
+  # Lists of the local and box files
+  bf <- box_dir_df$name[box_dir_df$type == "file"]
+  if(length(bf) < 1L)
+    return(NULL)
+  
+  lf <- gsub(".*/", "", loc_dir_df$name[loc_dir_df$type == "file"])
+  
+  absent  <- base::setdiff(bf, lf)
+  present <- base::intersect(bf, lf)
+  
+  # Do any of the existing files need updating?
+  to_update <- vector()
+  
+  if(length(lf) > 0L){
+    b_sha1 <- setNames(box_dir_df$sha1[box_dir_df$type == "file"], bf)
+    l_sha1 <- setNames(loc_dir_df$sha1[loc_dir_df$type == "file"], lf)
+    
+    to_update <- present[!l_sha1[present] == b_sha1[present]]
+  }
+  
+  # If overwrite is false, ignore to_update
+  if(!overwrite)
+    to_update <- ""
+  
+  # The ids of files to download
+  dl_ids <- setNames(box_dir_df$id, box_dir_df$name)[c(absent, to_update)]
+  
+  # Note, specifies filenames from the names of the dl_ids vector
+  # to write straight to disk
+  lapply(
+    dl_ids,
+    function(x)
+      box_dl(x, filename = names(x), overwrite = TRUE, local_dir)
+  )
+  return(TRUE)
+}
+
+#' @rdname downloadDirFiles
+uploadDirFiles <- function(dir_id, local_dir = getwd()){
+  
+  loc_dir_df <- create_loc_dir_df(local_dir)
+  box_dir_df <- box_ls(dir_id)
+  
+  # Lists of the local and box files
+  bf <- box_dir_df$name[box_dir_df$type == "file"]
+  
+  lf <- gsub(".*/", "", loc_dir_df$name[loc_dir_df$type == "file"])
+  if(length(lf) < 1L)
+    return(NULL)
+  
+  absent  <- base::setdiff(lf, bf)
+  present <- base::intersect(bf, lf)
+  
+  # Do any of the existing files need updating?
+  to_update <- vector()
+  
+  if(length(lf) > 0L){
+    b_sha1 <- setNames(box_dir_df$sha1[box_dir_df$type == "file"], bf)
+    l_sha1 <- setNames(loc_dir_df$sha1[loc_dir_df$type == "file"], lf)
+    
+    to_update <- present[!l_sha1[present] == b_sha1[present]]
+  }
+  
+  
+  update_names <- box_dir_df$name[box_dir_df$name %in% to_update]
+  update_ids   <- box_dir_df$id[box_dir_df$name %in% to_update]
+  
+  # Run through the files to update, and upload up dates
+  # NOTE: insert messages/progress bars here
+  ud_l <- 
+    mapply(
+      function(file_name, file_id)
+        box_update_file(
+          file.path(local_dir, file_name),
+          file_id,
+          dir_id
+        ),
+      update_names,
+      update_ids 
+    )
+  
+  # Run through the files to upload, and upload up dates
+  # NOTE: insert messages/progress bars here
+  ul_l <- 
+    lapply(
+      absent,
+      function(x)
+        box_upload_new(file.path(local_dir, x), dir_id)
+    )
+  
+  
+  return(TRUE)
+}
+
+
+
+#' Obtain a data.frame of the sub-directories in a box.com folder
+#' 
+#' Takes the \code{id} of a box folder and returns a data.frame of it's 
+#' subdirectories, indluding thier equivalent paths in the local directory.
+#' 
+#' @param dir_id The box.com id for the folder that you'd like to query
+#' @param local_dir The local directory which you'd like \code{dir_id} to 
+#' correspond to. If you're not interested in mapping local to hosted 
+#' directories, this isn't terribly important.
+#' @return A data.frame describing the contents directory structure of the 
+#' box.com folder corresponding to \code{dir_id}.
+#' @keywords internal
+dirTreeRecursive <- function(dir_id, local_dir = getwd()){
+  
+  dir_row <- 
+    data.frame(
+      type = "folder", id = dir_id, sequence_id = 0,
+      etag = 0, name = "",
+      local_dir = local_dir,
+      path = "~"
+    )
+  
+  dirDirs <- function(dir_id){
+    df <- box_ls(dir_id)
+    return(df[df$type == "folder",])
+  }
+  
+  dirTreeList <- function(dir_row){
+    sub_dirs <- dirDirs(dir_row[,"id"])
+    
+    if(!is.null(sub_dirs))
+      if(nrow(sub_dirs) > 0){
+        sub_dirs$local_dir <- paste0(dir_row[,"local_dir"], "/", sub_dirs$name)
+        sub_dirs$path      <- paste0(dir_row[,"path"], "/", sub_dirs$name)
+      }
+    
+    tmp <- list(sub_dirs)
+    
+    if(!is.null(sub_dirs))
+      if(nrow(sub_dirs) > 0)
+        for(i in 1:nrow(sub_dirs))
+          tmp <- c(tmp, Recall(sub_dirs[i,]))
+    tmp
+  }
+  
+  d <- dirTreeList(dir_row)
+  
+  do.call(rbind, d[!unlist(lapply(d, is.null))])
+}
+
+

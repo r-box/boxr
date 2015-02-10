85e88b9574f3edd9ad07a8e2cd996f1a24e44149
Brendan Rocks <rocks.brendan@gmail.com>
2015-02-10 10:32:11 -0800

    Added auth on .onAttach

--- R/zzz.R
+++ R/zzz.R
@@ -2,6 +2,7 @@
   # box_auth() # This causes an error, because httr::oauth_listener() needs
   # an interactive environment. You're best off writing a specific start-up
   # function
+  try(box_auth(interactive = FALSE, write.Renv = FALSE), silent = TRUE)
 }
 
 .onLoad <- function(libname, pkgname) {

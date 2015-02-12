b072120640dfa1e5d432186285b0ded354b57257
Brendan Rocks <rocks.brendan@gmail.com>
2015-02-11 17:05:34 -0800

    Basic print method fixed

--- R/boxr_s3_classes.R
+++ R/boxr_s3_classes.R
@@ -41,13 +41,45 @@ print.boxr_file_reference <- function(x, ...){
 # to the boxr expression called (e.g. upload call : box_ul(blah))
 #' @export
 print.boxr_dir_wide_operation_result <- function(x, ...){
-  cat("boxr", x$operation, "operation\n\n")
-#   cat("New files downloaded : ")
-#   print(data.frame(x$successful_downloads))
-#   cat("\nNew files uploaded : ")
-#   print(data.frame(x$successful_uploads))
-#   cat("\nUp-to-date files unchanged :")
-#   print(data.frame(x$up_to_date))
+  f <- x$file_list
   
+  cat("boxr", x$operation, "operation\n\n")
+  
+  cat(paste0(
+    "User : ", getOption("boxr.username")
+    ))
+  
+  print_df <- function(x, msg){
+    if(nrow(x) > 0){
+      cat(nrow(x), msg, ":\n")
+      print(
+        format(
+          setNames(data.frame(x), ""), 
+          justify = "left"
+        ), 
+        row.names = FALSE
+      )
+      cat("\n\n")
+    }
+  }
+  
+  # Run through the file df's in file_list, print out messages for them
+  dummy_var <- mapply(print_df, x$file_list, x$msg_list)
+  
+  cat("\nUse summary() to see individual file operations.")
   invisible(x)
 }
+
+
+
+# This will only really be shown for uploaded files. I can't think of a great
+# reason to explicitly 'map' this to local versions of a file at the moment.
+#
+# A better version of this would keep the whole httr call, in additon
+# to the boxr expression called (e.g. upload call : box_ul(blah))
+#' @export
+summary.boxr_dir_wide_operation_result <- function(x, ...){
+  print(x)
+}
+
+

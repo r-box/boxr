937667828c25da2cc11110fbe107edf895553dd3
Brendan Rocks <rocks.brendan@gmail.com>
2015-02-11 17:00:11 -0800

    Disable auto-login for now

--- R/zzz.R
+++ R/zzz.R
@@ -1,8 +1,5 @@
 .onAttach <- function(libname, pkgname) {
-  # box_auth() # This causes an error, because httr::oauth_listener() needs
-  # an interactive environment. You're best off writing a specific start-up
-  # function
-  try(box_auth(interactive = FALSE, write.Renv = FALSE), silent = TRUE)
+  # try(box_auth(interactive = FALSE, write.Renv = FALSE), silent = TRUE)
 }
 
 .onLoad <- function(libname, pkgname) {

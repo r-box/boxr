f6cc01cdfe2d9034b9598dca74662700c7b898e1
Brendan Rocks <rocks.brendan@gmail.com>
2015-04-14 23:23:28 -0700

    New tests for box_read wrapper functions (#29)

--- tests/testthat/test_05_read_write.R
+++ tests/testthat/test_05_read_write.R
@@ -35,15 +35,53 @@ test_that("You can write/read a remote .csv file", {
                    stringsAsFactors = FALSE)
   write.csv(df, tf, row.names = FALSE)
   
-  # Upload it, so that you can 'source it' back down
+  # Upload it, so that you can 'read it' back down
   b <- box_ul(0, tf)
   
-  # Can you source it in?
+  # box_read()
+  #
+  # Can you read it in?
   expect_message(df2 <- box_read(b$entries[[1]]$id), "read")
+  # Did the script execute correctly?
+  expect_equal(df, df2)
+  
+  # box_read_csv()
+  #
+  # Can you read it in?
+  expect_message(df2 <- box_read_csv(b$entries[[1]]$id), "read")
   # Did the script execute correctly?
   expect_equal(df, df2)
 })
 
+test_that("You can write/read a remote .tsv file", {
+  skip_on_cran()
+  boxr:::skip_on_travis()
+  
+  # Write a little .tsv file
+  tf <- paste0(tempfile(), ".tsv")
+  
+  df <- data.frame(a = letters[1:5], b = 1:5, c = rnorm(5), 
+                   stringsAsFactors = FALSE)
+  
+  write.table(df, tf, sep = "\t", col.names = TRUE, row.names = FALSE)
+  
+  # Upload it, so that you can 'read it' back down
+  b <- box_ul(0, tf)
+  
+  # box_read()
+  #
+  # Can you read it in?
+  expect_message(df2 <- box_read(b$entries[[1]]$id), "read")
+  # Did the script execute correctly?
+  expect_equal(df, df2)
+  
+  # box_read_tsv()
+  #
+  # Can you read it in?
+  expect_message(df2 <- box_read_tsv(b$entries[[1]]$id), "read")
+  # Did the script execute correctly?
+  expect_equal(df, df2)
+})
 
 test_that("You can write/read a remote .json file", {
   skip_on_cran()
@@ -57,11 +95,21 @@ test_that("You can write/read a remote .json file", {
   
   writeLines(jsonlite::toJSON(l), tf)
   
-  # Upload it, so that you can 'source it' back down
+  # Upload it, so that you can 'read it' back down
   b <- box_ul(0, tf)
   
-  # Can you source it in?
+  # box_read()
+  #
+  # Can you read it in?
   expect_message(l2 <- box_read(b$entries[[1]]$id), "read")
   # Did the script execute correctly?
   expect_equal(l, l2)
+  
+  # box_read_json()
+  #
+  # Can you read it in?
+  expect_message(l2 <- box_read_json(b$entries[[1]]$id), "read")
+  # Did the script execute correctly?
+  expect_equal(l, l2)
 })
+

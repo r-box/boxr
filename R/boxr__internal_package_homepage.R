c138403e240aed8e2945d4387116397a88872b6f
Brendan Rocks <rocks.brendan@gmail.com>
2015-05-21 18:41:41 -0700

    ?boxr 'homepage' (#31)

---
+++ R/boxr__internal_package_homepage.R
@@ -0,0 +1,21 @@
+#' boxr
+#' 
+#' A light-weight, high-level R interface for the box.com API
+#' 
+#' An introduction to the package and a short getting started guide (including 
+#' how to authorize boxr to work with your box.com account) can be found in the
+#' package's vignette. Simply run
+#' 
+#' \code{vignette("boxr")}
+#' 
+#' and it should open in your browser.
+#' 
+#' To see a list of the functions which \code{boxr} provides, click on the 
+#' "Index" hyperlink at the bottom of this page.
+#'
+#' @name boxr-package
+#' @aliases boxr
+#' @docType package
+#' @author Brendan Rocks \email{rocks.brendan@@gmail.com}
+#' @keywords package
+NULL

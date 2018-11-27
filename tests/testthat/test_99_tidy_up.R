de4b68f85e89dcae882a34af27a17b8dffc5b158
Nate <nathancday@gmail.com>
2018-11-26 23:03:14 -0500

    Tweaks to test (#84)
    * fix failing typo in fn_create(), tests pass now
    
    * added test-statements to empty/skipped test_02
    
    * wrapped test_99 into the skip_pattern to eliminate warning during TravisCI build, also added two test statments

--- tests/testthat/test_99_tidy_up.R
+++ tests/testthat/test_99_tidy_up.R
@@ -1,7 +1,20 @@
-# Tidying up --------------------------------------------------------------
+context("Tidying up")
 
-# Synch the highest level box.com dir with an empty local one to clear it out
-b <- box_push(0, "test_dir/dir_12/dir_121/dir_1211", delete = TRUE)
+test_that("Box directory is emptied", {
+  skip_on_cran()
+  boxr:::skip_on_travis()
+  
+  # push empty local dir to top level on Box
+  b <- box_push(0, "test_dir/dir_12/dir_121/dir_1211", delete = TRUE)
+  
+  expect_equal(nrow(as.data.frame(box_ls(0))), 0)
+})
 
-# Remove the local dir
-unlink("test_dir", recursive = TRUE, force = TRUE)
+test_that("Local directory is deleted", {
+  skip_on_cran()
+  boxr:::skip_on_travis()
+  
+  unlink("test_dir", recursive = TRUE, force = TRUE)
+  
+  expect_equal(sum(grepl("test_dir", list.dirs(recursive = F))), 0)
+})

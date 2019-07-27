2b81722d5fd22eebe26695e3931ef5e64cd3c561
Nate <nathancday@gmail.com>
2019-07-27 13:03:39 -0400

    new tests to evaluate if gargle is working on travis

--- tests/testthat/test_01_oauth.R
+++ tests/testthat/test_01_oauth.R
@@ -49,6 +49,16 @@ context("OAuth2.0 via JWT")
 #   expect_true(nchar(Sys.getenv("BOX_USER_ID")) > 8) # dunno about this number thing, but the IDs are long
 # })
 
+test_that("screte can be found", {
+  expect_true(gargle:::secret_can_decrypt("boxr"))
+})
+
+test_that("screte can be read", {
+  json <- gargle:::secret_read("boxr", "boxr-testing.json")
+  expect_class(json, "raw")
+  expect_true(length(json) > 50)
+})
+
 test_that("JWT works", {
   skip_if_no_token()
   expect_true(boxr_has_token())

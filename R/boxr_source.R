1737d9e25882138f1e8e34cc811e5b9fca539112
Ian Lyttle <ian.lyttle@schneider-electric.com>
2020-09-26 12:45:12 -0500

    Add caution to box_source(), make it internal so as not to publicize.

--- R/boxr_source.R
+++ R/boxr_source.R
@@ -1,5 +1,10 @@
 #' Source R code from a Box file
 #' 
+#' @description 
+#' **Note**: please use this function with extreme caution, as it is 
+#' possible for the contents of a Box file to change without
+#' your knowledge.
+#' 
 #' This function downloads a file from Box, then runs its 
 #' contents, as R code, using [source()]. 
 #' 
@@ -10,6 +15,7 @@
 #'   called for side-effect of modifying an environment.
 #' 
 #' @seealso [box_dl()], [box_save()], [box_read()] 
+#' @keywords internal
 #' @export
 #' 
 box_source <- function(file_id, local = globalenv(), ...) {

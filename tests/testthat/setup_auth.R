fc3c324632982eedda61fdc1d9cb5fb79071e097
Ian Lyttle <ian.lyttle@schneider-electric.com>
2021-01-31 17:58:11 -0600

    reconfigure Actions

--- tests/testthat/setup_auth.R
+++ tests/testthat/setup_auth.R
@@ -1,4 +1,12 @@
-use_token <- identical(Sys.getenv("BOXR_IGNORE_TOKEN"), "false")
+# To use this auth, you need an environment variable BOXR_PASSWORD.
+# If you are a maintainer of this repository, you have that environment variable.
+#
+# On GitHub Actions, it runs many on platforms simultaneously. We want only 
+# one of them to run this auth (to interact with the API), so we set another 
+# environment variable, BOXR_USE_TOKEN. We set this variable to `"true"` on 
+# the platform we want want to run this auth.
+#
+use_token <- identical(Sys.getenv("BOXR_USE_TOKEN"), "true")
 
 if (use_token && gargle:::secret_can_decrypt("boxr")) {
   raw <- gargle:::secret_read("boxr", "boxr-testing.json")

8833309c79684b5e9d60ee5abdb48e45e49d9e7a
Ian Lyttle <ijlyttle@me.com>
2024-01-07 14:08:54 -0600

    replace with exported functions from gargle

--- tests/testthat/setup_auth.R
+++ tests/testthat/setup_auth.R
@@ -8,9 +8,25 @@
 #
 use_token <- identical(Sys.getenv("BOXR_USE_TOKEN"), "true")
 
-if (use_token && gargle:::secret_can_decrypt("boxr")) {
-  raw <- gargle:::secret_read("boxr", "boxr-testing.json")
-  text <- rawToChar(raw)
+path_secret <- system.file("secret", "boxr-testing.json", package = "boxr")
+env_secret <- "BOXR_PASSWORD"
+
+# NOTE 2024-01-07: 
+#  - the key used with the deprecated gargle functions had 50 characters
+#  - the new gargle functions can accept a key max 32 characters
+#  - if you have an old "BOXR_PASSWORD", truncate it to use the first 32 chars 
+
+if (nchar(Sys.getenv(env_secret)) > 32) {
+  cli::cli_alert_warning(
+    paste(
+      "You may be using an older `BOXR_PASSWORD`.", 
+      "Truncate to use the first 32 characters."      
+    )
+  )
+}
+
+if (use_token && gargle::secret_has_key(env_secret)) {
+  text <- gargle::secret_decrypt_json(path_secret, env_secret)
   box_auth_service(token_text = text)
 }
 
